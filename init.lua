f272133878d42eec4f986bd1857304875d75da3c
green726 <mguinangreen726@gmail.com>
2022-02-11 17:06:27 -0500

    stuff

--- init.lua
+++ init.lua
@@ -6,7 +6,6 @@ require('plugins/bufferline')
 require('plugins/feline')
 require('plugins/indent-blanklines')
 require('plugins/git-signs')
-require('plugins/dev-icons')
 
 vim.opt.number = true
 vim.opt.encoding = 'UTF-8'
