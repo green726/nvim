1c76b7808115949cf44bc15ccf9bfa426e7eb4d3
green726 <mguinangreen726@gmail.com>
2022-02-11 10:47:17 -0500

    styff

---
+++ init.lua
@@ -0,0 +1,5 @@
+
+require('plugins/packer')
+
+require('monokai').setup { palette = require('monokai').pro }
+--require'lspconfig'.tsserver.setup{}
---
+++ lua/plugins/packer.lua
@@ -0,0 +1,7 @@
+return require('packer').startup(function()
+  use 'wbthomason/packer.nvim'
+  use 'tanvirtin/monokai.nvim'
+  use 'neovim/nvim-lspconfig' -- Collection of configurations for the built-in LSP client
+
+  
+  end)
