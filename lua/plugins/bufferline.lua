49f9803d445dd2a73ad0d0ecd835b690111eb877
green726 <mguinangreen726@gmail.com>
2022-02-11 22:58:50 -0500

    stuff

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,1 +1,25 @@
-require("bufferline").setup{}
+local present, bufferline = pcall(require, "bufferline")
+if not present then
+  return
+end
+
+bufferline.setup {
+  options = {
+    numbers = function(opts)
+      return string.format("%s", opts.id)
+    end,
+    diagnostics = "nvim_lsp",
+    offsets = {
+      {
+        filetype = "NvimTree",
+        text = "File Explorer",
+        highlight = "Directory",
+        text_align = "left"
+      },
+      {
+        filetype = "vista",
+        text = "LspTags",
+      }
+    }
+  }
+}
--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -3,6 +3,7 @@ return require('packer').startup(function()
   --theme
   use 'tanvirtin/monokai.nvim'
   --completion
+  --need to run this in powershell for java to work  curl https://projectlombok.org/downloads/lombok.jar -O E:\Coding\lombok\lombok.jar
   use {'neoclide/coc.nvim', branch = 'release'}
   --need to install glow withchoco
   --markdown viewer
@@ -16,8 +17,14 @@ return require('packer').startup(function()
   }
   --icons
   use 'kyazdani42/nvim-web-devicons'
-  --bufferline
-  use {'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons'}
+  -- bufferline
+  use {
+    "akinsho/nvim-bufferline.lua",
+    after = "nvim-web-devicons",
+    config = function()
+      require("plugins/bufferline")
+    end
+  }
   --statusline
   use 'feline-nvim/feline.nvim'
   --indents
