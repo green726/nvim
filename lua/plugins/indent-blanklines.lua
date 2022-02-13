1ea393cf9827dd991d1c77f6b27b726c48696cae
green726 <59667714+green726@users.noreply.github.com>
2022-02-12 19:59:09 -0500

    got fonts

--- lua/plugins/indent-blanklines.lua
+++ lua/plugins/indent-blanklines.lua
@@ -1,25 +1,31 @@
------------------------------------------------------------
--- Indent line configuration file
------------------------------------------------------------
+-- indent-blankline character.
 
--- Plugin: indent-blankline
--- https://github.com/lukas-reineke/indent-blankline.nvim
+local indent_blankline_styles = {
+    "▏",
+    "¦",
+    "┆",
+    "│",
+    "⎸",
+    "|",
+}
 
+vim.g.indent_blankline_char = indent_blankline_styles[1]
 
-require('indent_blankline').setup {
-    char = "▏",
-    show_first_indent_level = false,
-    filetype_exclude = {
-      'help',
-      'git',
-      'markdown',
-      'text',
-      'terminal',
-      'lspinfo',
-      'packer'
-    },
-    buftype_exclude = {
-      'terminal',
-      'nofile'
-    },
-  }
+-- Disable indent-blankline on these pages.
+vim.g.indent_blankline_filetype_exclude = {
+  "help",
+  "terminal",
+  "dashboard",
+  "packer",
+  "lsp-installer",
+  "lspinfo",
+  "vista_kind",
+  "alpha"
+}
+vim.g.indent_blankline_buftype_exclude = { "terminal" }
+
+vim.g.indent_blankline_show_trailing_blankline_indent = true
+vim.g.indent_blankline_show_first_indent_level = true
+vim.g.indent_blankline_show_current_context = true
+vim.g.indent_blankline_context_char = indent_blankline_styles[indent_blankline_style]
+vim.cmd("autocmd CursorMoved * IndentBlanklineRefresh")
--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -19,7 +19,7 @@ return require('packer').startup(function(use)
   --icons
   use 'kyazdani42/nvim-web-devicons'
   -- bufferline
-  use {'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons'}
+  use {'akinsho/bufferline.nvim'}
   --statusline
   use 'feline-nvim/feline.nvim'
   --indents
@@ -35,10 +35,7 @@ return require('packer').startup(function(use)
   }
   --file tree 
   use {
-    'kyazdani42/nvim-tree.lua',
-    requires = {
-      'kyazdani42/nvim-web-devicons', -- optional, for file icon
-    }
+    'kyazdani42/nvim-tree.lua'
   }
   --home menu/start
   use 'goolord/alpha-nvim'
