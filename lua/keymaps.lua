46f8015496ece2b17afe7d9be1b1e4de37963b42
green726 <mguinangreen726@gmail.com>
2022-03-06 22:02:35 -0500

    coc stuff

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -20,6 +20,13 @@ map("n", "ff", ":Telescope find_files<CR>", { silent = true })
 
 map("n", "<C-v>", ":NvimTreeFocus<CR>", { silent = true })
 map("n", "<C-b>", ":NvimTreeClose<CR>", { silent = true })
+
+--coc stuff
+vim.api.nvim_set_keymap("n", "gd", "<Plug>(coc-definition)", {silent = true})
+vim.api.nvim_set_keymap("n", "<leader>rn", "<Plug>(coc-rename)", {})
+
+
+
 --[[
 var io = require('socket.io')(http, { 
     cors: {
