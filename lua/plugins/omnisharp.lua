c61686fe549241853118736768d5ad3d4a3e05c5
green726 <59667714+green726@users.noreply.github.com>
2022-03-09 17:57:06 -0500

    eee

--- lua/plugins/omnisharp.lua
+++ lua/plugins/omnisharp.lua
@@ -1,21 +1,6 @@
-vim.g["OmniSharp_highlighting"] = 0;
---[[
-vim.g["OmniSharp_highlighting_groups"] = {
-    'ExcludedCode': 'NonText',
-    'ClassName': 'Typedef',
-    'StructName': 'Typedef',
-    'DelegateName': 'Structure',
-    'EnumName': 'Structure',
-    'InterfaceName': 'Structure',
-    'ModuleName': 'Structure',
-    'NamespaceName': 'Include',
-    'RegexComment': 'Comment',
-    'RegexCharacterClass': 'Character',
-    'RegexAnchor': 'Type',
-    'RegexQuantifier': 'Number',
-    'RegexGrouping': 'Macro',
-    'RegexAlternation': 'Identifier',
-    'RegexText': 'String',
-    'RegexSelfEscapedCharacter': 'Delimiter',
-    'RegexOtherEscape': 'Delimiter'
-};]]--
+vim.g["OmniSharp_highlighting"] = 0
+
+vim.g["OmniSharp_popup_position"] = "peek"
+vim.g["OmniSharp_popup_options"] = {
+    winhl = "Normal:NormalFloat"
+}
