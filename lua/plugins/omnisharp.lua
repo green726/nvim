bcc2d3af945738711c6643653a13d63e6879f3d9
green726 <mguinangreen726@gmail.com>
2022-03-11 16:47:01 -0500

    Update omnisharp.lua

--- lua/plugins/omnisharp.lua
+++ lua/plugins/omnisharp.lua
@@ -3,7 +3,7 @@ vim.g["OmniSharp_highlighting"] = 0
 vim.g["OmniSharp_popup_position"] = "atcursor"
 vim.g["OmniSharp_popup_options"] = {
     winhl = "Normal:Normal",
-    winblend = 30
+    winblend = 15
 }
 
 vim.g["OmniSharp_popup_mappings"] = {
