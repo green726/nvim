08aa40ccbc5ba4b1519ac357fee3577ff1997484
green726 <59667714+green726@users.noreply.github.com>
2022-03-11 10:14:35 -0500

    omnisharp docs is better now

--- init.lua
+++ init.lua
@@ -9,12 +9,26 @@ vim.opt.number = true
 vim.opt.encoding = 'UTF-8'
 
 --first line of code below is to prevent screen tearing
---below comment can be added for highlight on cursor hold
+--below comments are to remember stuff - first is call docs on cursor hold, second is echo char under cursor
 --autocmd CursorHold *.cs :OmniSharpDocumentation
+--autocmd CursorHold *.cs :echo matchstr(getline('.'), '\%' . col('.') . 'c.')
 vim.cmd [[
+    func CsDocs()
+      let wordsIgnore = ['', ' ', '(', ')', '{', '}', ';', 'public', 'static', 'private', 'void']
+      let word = expand("<cword>")
+      for i in wordsIgnore
+        if word == i
+          return
+        endif
+      endfor
+      :OmniSharpDocumentation
+    endfunc
+
     autocmd BufEnter * highlight Normal guibg=0
     autocmd CursorHold *.java :call CocAction('doHover')
     autocmd BufWrite *.cs :OmniSharpCodeFormat
+    autocmd CursorHold *.cs :call CsDocs()
+
 
 ]]
 
