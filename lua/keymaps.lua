cdce39e8cac7a3f1b86540905205db1ca4bc7634
green726 <59667714+green726@users.noreply.github.com>
2022-03-20 17:19:50 -0400

    made a bit better

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,9 +1,9 @@
 function map(mode, lhs, rhs, opts)
-    local options = { noremap = true }
-    if opts then
-        options = vim.tbl_extend("force", options, opts)
-    end
-    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+	local options = { noremap = true }
+	if opts then
+		options = vim.tbl_extend("force", options, opts)
+	end
+	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
 map("n", "]b", ":BufferLineCycleNext<CR>", { silent = true })
@@ -22,27 +22,30 @@ map("n", "<leader>t", ":ToggleTerm<CR>", { silent = true })
 
 map("n", "ff", ":Telescope find_files<CR>", { silent = true })
 
-map("n", "<C-t>", ":NvimTreeFocus<CR>", { silent = true })
-map("n", "<C-j>", ":NvimTreeClose<CR>", { silent = true })
+map("n", "<C-t>", ":NvimTreeToggle<CR>", { silent = true })
+-- map("n", "<C-j>", ":NvimTreeClose<CR>", { silent = true })
 
 map("n", "cf", ":CocFix<CR>", { silent = true })
 
 --context menu
 map("n", "<C-m>", ":ConMenu<CR>", { silent = true })
 
-vim.api.nvim_set_keymap("n", "<leader>gu", ':lua dofile("C:/Users/mguin/AppData/Local/nvim/lua/plugins/toggleTermConfig.lua")() <CR>', {silent = true})
-vim.api.nvim_set_keymap("n", "tt", ":ToggleTerm <CR>", {silent = true})
-vim.api.nvim_set_keymap("n", "<leader>set", ":e C:/Users/mguin/AppData/Local/nvim <CR>", {silent = true})
-
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>gu",
+	':lua dofile("C:/Users/mguin/AppData/Local/nvim/lua/plugins/toggleTermConfig.lua")() <CR>',
+	{ silent = true }
+)
+vim.api.nvim_set_keymap("n", "tt", ":ToggleTerm <CR>", { silent = true })
+vim.api.nvim_set_keymap("n", "<leader>set", ":e C:/Users/mguin/AppData/Local/nvim <CR>", { silent = true })
 
 --coc stuff
-vim.api.nvim_set_keymap("n", "gd", "<Plug>(coc-definition)", {silent = true})
+vim.api.nvim_set_keymap("n", "gd", "<Plug>(coc-definition)", { silent = true })
 vim.api.nvim_set_keymap("n", "<leader>rn", "<Plug>(coc-rename)", {})
 vim.api.nvim_set_keymap("n", "<leader>osrn", "<Plug>(omnisharp_rename)", {})
 vim.api.nvim_set_keymap("n", "oh", "<Plug>(omnisharp_do<C-k>mentation)", {})
 
-
-vim.cmd [[
+vim.cmd([[
   if has('nvim-0.4.0') || has('patch-8.2.0750')
   echo 'hi'
   nnoremap <silent><nowait><expr> <C-f> coc#float#has_scroll() ? coc#float#scroll(1) : "\<C-f>"
@@ -52,10 +55,9 @@ vim.cmd [[
   vnoremap <silent><nowait><expr> <C-f> coc#float#has_scroll() ? coc#float#scroll(1) : "\<C-f>"
   vnoremap <silent><nowait><expr> <C-b> coc#float#has_scroll() ? coc#float#scroll(0) : "\<C-b>"
   endif
-]]
+]])
 
 --map("n", "<C-d>", ":call CocAction('doHover')<CR>", {silent = true})
-
 
 --[[
 var io = require('socket.io')(http, { 
