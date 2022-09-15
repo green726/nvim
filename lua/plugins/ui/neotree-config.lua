6cca244441ff04e13a426bc380d194df2977a7a0
Milan Lustig <mguinangreen726@gmail.com>
2022-09-15 09:41:57 -0400

    added telescope to neotree

--- lua/plugins/ui/neotree-config.lua
+++ lua/plugins/ui/neotree-config.lua
@@ -1,3 +1,25 @@
+local function getTelescopeOpts(state, path)
+    return {
+        cwd = path,
+        search_dirs = { path },
+        attach_mappings = function(prompt_bufnr, map)
+            local actions = require "telescope.actions"
+            actions.select_default:replace(function()
+                actions.close(prompt_bufnr)
+                local action_state = require "telescope.actions.state"
+                local selection = action_state.get_selected_entry()
+                local filename = selection.filename
+                if (filename == nil) then
+                    filename = selection[1]
+                end
+                -- any way to open the file without triggering auto-close event of neo-tree?
+                require("neo-tree.sources.filesystem").navigate(state, state.path, filename)
+            end)
+            return true
+        end
+    }
+end
+
 require("neo-tree").setup({
     close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
     popup_border_style = "single",
@@ -151,14 +173,28 @@ require("neo-tree").setup({
                 ["<bs>"] = "navigate_up",
                 ["."] = "set_root",
                 ["H"] = "toggle_hidden",
-                ["/"] = "fuzzy_finder",
+                ["ff"] = "telescope_finder",
+                ["fs"] = "telescope_grep",
+                ["f"] = "",
                 ["D"] = "fuzzy_finder_directory",
-                ["f"] = "filter_on_submit",
+                ["/"] = "filter_on_submit",
                 ["<c-x>"] = "clear_filter",
                 ["[g"] = "prev_git_modified",
                 ["]g"] = "next_git_modified",
             }
         }
+    },
+    commands = {
+        telescope_find = function(state)
+            local node = state.tree:get_node()
+            local path = node:get_id()
+            require('telescope.builtin').find_files(getTelescopeOpts(state, path))
+        end,
+        telescope_grep = function(state)
+            local node = state.tree:get_node()
+            local path = node:get_id()
+            require('telescope.builtin').live_grep(getTelescopeOpts(state, path))
+        end,
     },
     buffers = {
         follow_current_file = true, -- This will find and focus the file in the active buffer every
