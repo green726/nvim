565ec3137255a7014fe2ec15e5d5066d687bddbc
Milan Lustig <mguinangreen726@gmail.com>
2023-08-26 00:14:05 -0400

    delay train doesnt work in visual :( - it fully disables keys so i had to disable delays in visual mode

--- lua/plugins/misc/delay-train-config.lua
+++ lua/plugins/misc/delay-train-config.lua
@@ -1,8 +1,9 @@
 require('delaytrain').setup {
-    delay_ms = 700,      -- How long repeated usage of a key should be prevented
-    grace_period = 2,     -- How many repeated keypresses are allowed
+    delay_ms = 1000,      -- How long repeated usage of a key should be prevented
+    grace_period = 3,     -- How many repeated keypresses are allowed
     keys = {              -- Which keys (in which modes) should be delayed
-        ['nv'] = { 'h', 'j', 'k', 'l', 'b', 'w'},
+        ['n'] = { 'h', 'j', 'k', 'l', 'b', 'w'},
+        -- ['v'] = { 'h', 'j', 'k', 'l', 'b', 'w'},
         ['nvi'] = { '<Left>', '<Down>', '<Up>', '<Right>' },
     },
     ignore_filetypes = {},     -- Example: set to {"help", "NvimTr*"} to
